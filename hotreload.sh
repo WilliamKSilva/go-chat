e5ae1144dfb6978da7ffab9276cc5d768d809b3e
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-02-01 17:33:53 -0300

    feat: add bash script to re-interpret server everytime an change is made

--- hotreload.sh
+++ hotreload.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
-GIT='git --git-dir='$PWD'/.git'
 
-CHANGES=$($GIT diff)
-echo "${CHANGES}"
+# Use this for development purposes only #
+# Its so boring to call GO interpreter everytime # 
+
+ROOTDIR=$(dirname "$0")
+ENTRYPOINT_SERVER="${ROOTDIR}/cmd/server/main.go"
+GIT='git --git-dir='$PWD'/.git'
+GOCOMPILER='/usr/local/go/bin/go'
+
+LAST_CHANGES_SIZE=0
+while true
+do
+    CHANGES=$($GIT diff)
+    CHANGES_OUTPUT_LENGTH=$(printf "%s" "$CHANGES" | wc -c)
+    if (($CHANGES_OUTPUT_LENGTH == $LAST_CHANGES_SIZE));
+    then
+        continue
+    fi
+
+    LAST_CHANGES_SIZE=$CHANGES_OUTPUT_LENGTH
+
+    if (($CHANGES_OUTPUT_LENGTH > 0));
+    then
+        echo "Recompiled!"
+        $($GOCOMPILER run $ENTRYPOINT_SERVER) &
+    fi
+done
