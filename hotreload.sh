e5e08dd3be338a59be505f0bae34dd9ade1b32e0
WilliamKSilva <wilz.kelvin12@gmail.com>
2024-02-02 16:07:49 -0300

    fix: change recompile hotreload script condition

--- hotreload.sh
+++ hotreload.sh
@@ -21,7 +21,7 @@ do
 
     LAST_CHANGES_SIZE=$CHANGES_OUTPUT_LENGTH
 
-    if [ "$FIRST_COMPILE_DONE" != "true" ] || [ "$CHANGES_OUTPUT_LENGTH" -gt 0 ]; then
+    if (( !$FIRST_COMPILE_DONE )) || (($CHANGES_OUTPUT_LENGTH > 1000)); then
         if [ "$FIRST_COMPILE_DONE" != "true" ]; then
             FIRST_COMPILE_DONE=true
         fi
